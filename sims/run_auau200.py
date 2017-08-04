957c70d8438820531c8dfa62678aa34ec8ed8f6c
Ron Belmont <belmonrj@gmail.com>
2017-08-04 12:45:44 -0400

    10fm went to 200 tracks, trying 12 fm

--- sims/run_auau200.py
+++ sims/run_auau200.py
@@ -64,7 +64,7 @@ target.write("79\n")					# IZP (projectile Z number)
 target.write("197\n")					# IAT (target A number)     Au: 197 Pb: 208
 target.write("79\n")					# IZT (target Z number)     Au: 79  Pb: 82
 target.write("{}\n".format(nevents))	                # NEVNT (total number of events)
-target.write("10.0\n")					# BMIN (mininum impact parameter in fm)
+target.write("12.0\n")					# BMIN (mininum impact parameter in fm)
 target.write("20.0\n")					# BMAX (maximum impact parameter in fm, also see below)
 target.write("4\n")					# ISOFT (D=1): select Default AMPT or String Melting(see below)
 target.write("150\n")					# NTMAX: number of timesteps (D=150), (D=3 off cascade)        -------
